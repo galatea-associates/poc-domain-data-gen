72bc72f17f2c9124698c1d5609d3f748f1151377
James Turner <JamesTurner@Galatea.com>
2019-07-10 16:33:58 +0100

    Prefix dummy fields with domain object name

--- src/domainobjects/counterparty.py
+++ src/domainobjects/counterparty.py
@@ -6,14 +6,14 @@ class Counterparty(Generatable):
     def get_template(self, data_generator):
         return {
             'counterparty_id': {'func': data_generator.generate_rdn, 'field_type': 'id'},
-            'field1': {'func': data_generator.generate_rdn},
-            'field2': {'func': data_generator.generate_rdn},
-            'field3': {'func': data_generator.generate_rdn},
-            'field4': {'func': data_generator.generate_rdn},
-            'field5': {'func': data_generator.generate_rdn},
-            'field6': {'func': data_generator.generate_rdn},
-            'field7': {'func': data_generator.generate_rdn},
-            'field8': {'func': data_generator.generate_rdn},
-            'field9': {'func': data_generator.generate_rdn},
-            'field10': {'func': data_generator.generate_rdn}
+            'counterparty_field1': {'func': data_generator.generate_rdn},
+            'counterparty_field2': {'func': data_generator.generate_rdn},
+            'counterparty_field3': {'func': data_generator.generate_rdn},
+            'counterparty_field4': {'func': data_generator.generate_rdn},
+            'counterparty_field5': {'func': data_generator.generate_rdn},
+            'counterparty_field6': {'func': data_generator.generate_rdn},
+            'counterparty_field7': {'func': data_generator.generate_rdn},
+            'counterparty_field8': {'func': data_generator.generate_rdn},
+            'counterparty_field9': {'func': data_generator.generate_rdn},
+            'counterparty_field10': {'func': data_generator.generate_rdn}
         }
