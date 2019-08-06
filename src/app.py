c8784cd2930948535ee62fcdaeeb24fa5a020710
Joe Bellott <jbellott@Galatea.com>
2019-08-06 11:10:56 +0100

    Removed cache configuration from app.py

--- src/app.py
+++ src/app.py
@@ -5,9 +5,21 @@ import os
 import timeit
 import sqlite3
 import logging
-import pandas as pd
 from cache import Cache
 from sqlite_database import Sqlite_Database
+
+def get_file_builder_config(file_builders, file_builder_name):
+    return list(filter(lambda file_builder: file_builder['name'] == file_builder_name, file_builders))[0]
+
+def get_file_builder(file_builder_config):
+    return getattr(importlib.import_module('filebuilders.' + file_builder_config['module_name']), file_builder_config['class_name'])    
+
+# Configure a parser for command line argument retrieval, and retrieve said arguments 
+def get_args():
+    parser = argparse.ArgumentParser(description='Generate Random Data for Various Domain Objects')
+    parser.add_argument('--config', default='src/config.json', help='JSON Config File Location')
+    cl_args = parser.parse_args()
+    return cl_args
 
 def process_domain_object(domain_obj_config, cache, dependency_db,file_builder):
     domain_obj_class = getattr(importlib.import_module('domainobjects.' + domain_obj_config['module_name']), domain_obj_config['class_name'])
@@ -16,44 +28,26 @@ def process_domain_object(domain_obj_config, cache, dependency_db,file_builder):
     custom_args = domain_obj_config['custom_args']
     domain_obj.generate(record_count, custom_args, domain_obj_config, file_builder)
 
-def get_file_builder_config(file_builders, file_builder_name):
-    return list(filter(lambda file_builder: file_builder['name'] == file_builder_name, file_builders))[0]
-
-def get_file_builder(file_builder_config):
-    return getattr(importlib.import_module('filebuilders.' + file_builder_config['module_name']), file_builder_config['class_name'])    
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', default=os.path.join('src', 'config.json'), help='JSON config file location')
-    return parser.parse_args()
-
-def initialise_cache(cache):
-    cache.persist_to_cache('currencies', ['USD', 'CAD', 'EUR', 'GBP'])
-    cache.persist_to_cache('tickers', pd.read_csv('tickers.csv')['Symbol'].drop_duplicates().values.tolist())
-    cache.persist_to_cache('exchange_codes', ['L', 'N', 'OQ', 'SI', 'AL', 'VI', 'BB', 'BM', 'BR', 'BG', 'TC', 'TO', 'HK', 'SS',
-                                    'FR', 'BE', 'DE', 'JA', 'DE', 'IL', 'VX', 'MFM', 'PA', 'ME', 'NZ'])
-    cache.persist_to_cache('cois', ['US', 'GB', 'CA', 'FR', 'DE', 'CH', 'SG', 'JP'])
-
-
 def main():
     start_time = timeit.default_timer()
-    cache = Cache()
-    initialise_cache(cache)
-    dependency_db = Sqlite_Database()
     
-    args = get_args()  
+    
+    cache = Cache()                     # Stores global generation attributes, i.e: tickers, countries of issuance, exchange codes etc.
+    dependency_db = Sqlite_Database()   # Stores global generation dependencies, i.e instrument RICs. 
+    args = get_args()                   # Stores command line arguments TODO: CHECK THIS IS RIGHT
 
     with open(args.config) as config_file:
         config = json.load(config_file)
 
-    domain_objects = config['domain_objects']
-    file_builders = config['file_builders']
+    domain_object_configs = config['domain_objects']
+    file_builder_configs = config['file_builders']
 
-    for domain_object in domain_objects:
-        file_builder_config = get_file_builder_config(file_builders, domain_object['file_builder_name'])      
-        file_builder = get_file_builder(file_builder_config)(None, domain_object['output_directory'], domain_object['file_name'], file_builder_config['file_extension']) 
-        process_domain_object(domain_object, cache, dependency_db, file_builder)
+    for domain_object_config in domain_object_configs:
+        file_builder_config = get_file_builder_config(file_builder_configs, domain_object_config['file_builder_name'])      
+        file_builder = get_file_builder(file_builder_config)(None, domain_object_config['output_directory'], domain_object_config['file_name'], file_builder_config['file_extension']) 
+        process_domain_object(domain_object_config, cache, dependency_db, file_builder)
         dependency_db.commit_changes()
+    
     end_time = timeit.default_timer()
     print("Run time: ", end_time-start_time)
 
