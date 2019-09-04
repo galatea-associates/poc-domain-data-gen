6508c69c717ed045798184ee39273f175e47e11b
Joe Bellott <jbellott@Galatea.com>
2019-09-04 15:43:32 +0100

    Hound fixes

--- src/app.py
+++ src/app.py
@@ -6,40 +6,41 @@ import logging
 import random
 from cache import Cache
 from sqlite_database import Sqlite_Database
-#from GoogleDriveAccessor import GoogleDriveAccessor
+# from GoogleDriveAccessor import GoogleDriveAccessor
+
 
 # Return a specified class from package.module
 def get_class(package_name, module_name, class_name):
-    return getattr(importlib.import_module(package_name+'.'+module_name), 
-                    class_name)
+    return getattr(importlib.import_module(package_name+'.'+module_name),
+           class_name)
 
-# Create list comprehension of the file builder config which matches a provided name
+# Create list comprehension of the file builder config which matches a name
 def get_file_builder_config(file_builders, file_builder_name):
     return list(filter(
-            lambda file_builder: file_builder['name'] == file_builder_name, 
+            lambda file_builder: file_builder['name'] == file_builder_name,
             file_builders
             ))[0]
 
 # Facillitate the domain object generation procedure
-def process_domain_object(domain_obj_config, cache, 
+def process_domain_object(domain_obj_config, cache,
                           dependency_db, file_builder):
 
-    domain_obj_class = get_class('domainobjects', 
-                                 domain_obj_config['module_name'], 
+    domain_obj_class = get_class('domainobjects',
+                                 domain_obj_config['module_name'],
                                  domain_obj_config['class_name'])
 
-    domain_obj = domain_obj_class(cache, dependency_db, file_builder, 
+    domain_obj = domain_obj_class(cache, dependency_db, file_builder,
                                   domain_obj_config)
 
     record_count = int(domain_obj_config['record_count'])
     custom_args = domain_obj_config['custom_args']
     domain_obj.generate(record_count, custom_args)
 
-# Configure a parser for command line argument retrieval, and retrieve arguments 
+# Configure a parser for command line argument retrieval, and retrieve arguments
 def get_args():
-    parser = argparse.ArgumentParser(description='''Generate Random Data for 
-                                     various Domain Objects''')
-    parser.add_argument('--config', default='src/config.json', 
+    parser = argparse.ArgumentParser(description='''Generate Random Data for
+                                      various Domain Objects''')
+    parser.add_argument('--config', default='src/config.json',
                         help='JSON Config File Location')
     cl_args = parser.parse_args()
     return cl_args
@@ -50,13 +51,13 @@ def main():
     # random.seed(100)
 
     start_time = timeit.default_timer()
-    logging.basicConfig(filename='generator.log', filemode='w', 
-                        format='%(levelname)s : %(message)s', 
+    logging.basicConfig(filename='generator.log', filemode='w',
+                        format='%(levelname)s : %(message)s',
                         level=logging.INFO)
 
     cache = Cache()                     # Store global generation attributes
     dependency_db = Sqlite_Database()   # Store global generation dependencies
-    args = get_args()                   # Stores command line arguments 
+    args = get_args()                   # Stores command line arguments
     # google_drive_accessor = GoogleDriveAccessor(args.g_drive_root)
 
     with open(args.config) as config_file:
