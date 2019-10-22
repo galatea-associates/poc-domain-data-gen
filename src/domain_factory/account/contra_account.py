9ffc43f8d25ad4f6a52e8b23582688b0995db972
Tolu Onasanya <tonasanya@Galatea.com>
2019-10-22 11:51:49 +0100

    contra line length was too long.

--- src/domain_factory/account/contra_account.py
+++ src/domain_factory/account/contra_account.py
@@ -4,8 +4,8 @@ from domain_factory.account.external_counterparty_account import ECPAccount
 class ContraAccount(ECPAccount):
     """ Class to generate contra ecp accounts. Generate method will call the
     ECPAccount generate method in order to populate fields shared by all ECP
-    accounts. The generate method in this class will then populate the remaining
-    attributes unique to the Contra Account domain factory. """
+    accounts. The generate method in this class will then populate the
+    remaining attributes unique to the Contra Account domain factory. """
 
     def generate(self, record_count, start_id):
         """ Generate a set number of Contra accounts
