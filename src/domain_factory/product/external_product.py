6cc73a7f2c537dcbc30fc9685cb7f1f6000d5c26
Tolu Onasanya <onasanya@galatea-associates.com>
2019-10-23 16:32:21 +0100

    product domain structure added

---
+++ src/domain_factory/product/external_product.py
@@ -0,0 +1,28 @@
+from domain_factory.product.product import Product
+
+
+class ExternalProduct(Product):
+    """ Class to generate external products. Generate method will call the
+    Product generate method in order to populate fields shared by all
+    products. The generate method in this class will then populate the
+    remaining attributes unique to the External Product domain factory. """
+
+    def generate(self, record_count, start_id):
+        """ Generate a set number of products
+
+            Parameters
+            ----------
+            record_count : int
+                Number of External Products to generate
+            start_id : int
+                Starting id to generate from
+
+            Returns
+            -------
+            List
+                Containing 'record_count' External Products
+            """
+
+        records = []
+
+        return records
