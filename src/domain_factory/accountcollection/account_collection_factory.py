68659a27c87724b907939f0bf83246877bf74e74
Thomas Edwards <edwards@galatea-associates.com>
2019-10-24 18:39:23 +0100

    #92 Update dependencies and add attributes to account_collection_factory

--- src/domain_factory/accountcollection/account_collection_factory.py
+++ src/domain_factory/accountcollection/account_collection_factory.py
@@ -1,9 +1,9 @@
 from abc import abstractmethod
 
-from domain_factory.generatable import Generatable
+from domain_factory.factory import Factory
 
 
-class AccountCollectionFactory(Generatable):
+class AccountCollectionFactory(Factory):
     """ Abstract Class to generate Account Collections.  Generate method will
     generate the attributes shared by all account collection factory child
     classes.
@@ -38,3 +38,42 @@ class AccountCollectionFactory(Generatable):
             """
 
         pass
+
+    def generate_record(self, id):
+        """ Generate a single account collection
+
+        Returns
+        -------
+        dict
+            A single account collection object
+        """
+
+        return {
+            'account_collection_id': id,
+            'creation_time_stamp': self.generate_timestamp(),
+            'updated_time_stamp': self.generate_timestamp(),
+            'account_collection_short_name': self.generate_short_name(),
+            'description': self.generate_description()
+        }
+
+    def generate_short_name(self):
+        """ Generate a short name
+
+        Returns
+        -------
+        string
+            A short name
+        """
+
+        return "account short name"
+
+    def generate_description(self):
+        """ Generate a description
+
+        Returns
+        -------
+        string
+            A Description
+        """
+
+        return "account description"
