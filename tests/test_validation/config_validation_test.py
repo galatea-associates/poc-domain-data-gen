3c224d4ab69f9c9cb9a19a7042407dc9640f13e0
Tolu Onasanya <onasanya@galatea-associates.com>
2019-10-29 11:27:00 +0000

    Revert "Config Validation Testing Finished"
    This reverts commit d79cb4b57857d89557f3a09b9bb0233484330a7a.

---
+++ tests/test_validation/config_validation_test.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.insert(0, 'src/')
+from src.validator import config_validator as validator
+import json
+
+
+def test_record_count():
+    run_test("record_count")
+
+
+def test_file_size():
+    run_test("file_size")
+
+
+def test_file_extension():
+    run_test("job_size")
+
+
+def test_pool_size():
+    run_test("pool_size")
+
+
+def test_job_size():
+    run_test("job_size")
+
+
+def run_test(test_name):
+    success_config = load_file(test_name+"_success.json")
+    failure_config = load_file(test_name+"_fail.json")
+    success_result = run_validation(success_config).check_success()
+    fail_result = run_validation(failure_config).check_success()
+    assert success_result and not fail_result
+
+def load_file(file_name):
+
+
+def run_validation(config_file):
+    return validator.validate(config_file)
+
+def retrieve_configs():
+    return ['testingthetest']
