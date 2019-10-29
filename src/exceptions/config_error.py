c58d83b905e303206de2dacfa57a80c78c8a221d
Tolu Onasanya <onasanya@galatea-associates.com>
2019-10-29 11:20:38 +0000

    Revert "Merge pull request #81 from galatea-associates/feature/issue-55-config-validation-new-config"
    This reverts commit 20fb67d465167b335de748c82c266273dbdc9eda, reversing
    changes made to 5d01279045363edc9f03031f9f8d23ec05db71a5.

--- src/exceptions/config_error.py
+++
@@ -1,8 +0,0 @@
-class ConfigError(Exception):
-    """ Raised when there is an issue with the user-defined configuration.
-    Due to prior checks compiling a list of these, all are returned and
-    displayed to the user.
-    """
-
-    def __init__(self):
-        pass
