85343b4f21af26a1021e41fb11c83ce74d409dbd
Tolu Onasanya <onasanya@galatea-associates.com>
2019-10-29 11:23:51 +0000

    Revert "python code style on new classes"
    This reverts commit eee71f10cb8384aa36f2517ce7c12acf98b9d388.

--- src/domain_factory/account/external_counterparty_account.py
+++ src/domain_factory/account/external_counterparty_account.py
@@ -4,28 +4,28 @@ from domain_factory.account.brokerage_account import BrokerageAccount
 
 
 class ECPAccount(BrokerageAccount):
-    """ Abstract class to generate ECP accounts. Generate method will call the
-   BrokerageAccount generate method in order to populate fields shared by all
-   Brokerage accounts. The generate method in this class will then populate the
-   remaining attributes unique to the ECP Account domain factory. """
+  """ Abstract class to generate ECP accounts. Generate method will call the
+ BrokerageAccount generate method in order to populate fields shared by all
+ Brokerage accounts. The generate method in this class will then populate the
+ remaining attributes unique to the ECP Account domain factory. """
 
-    @abstractmethod
-    def generate(self, record_count, start_id):
-        """ Generate a set number of ECP accounts
+  @abstractmethod
+  def generate(self, record_count, start_id):
+    """ Generate a set number of ECP accounts
 
-            Parameters
-            ----------
-            record_count : int
-                Number of ecp accounts to generate
-            start_id : int
-                Starting id to generate from
+        Parameters
+        ----------
+        record_count : int
+            Number of ecp accounts to generate
+        start_id : int
+            Starting id to generate from
 
-            Returns
-            -------
-            List
-                Containing 'record_count' ecp accounts
-            """
+        Returns
+        -------
+        List
+            Containing 'record_count' ecp accounts
+        """
 
-        records = []
+    records = []
 
-        return records
+    return records
