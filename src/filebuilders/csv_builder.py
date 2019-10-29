2adf150bc1291ecb62600e82919aeb801ed85918
Tolu Onasanya <onasanya@galatea-associates.com>
2019-10-29 11:36:36 +0000

    Revert "Merge pull request #42 from galatea-associates/feature/issue_10_swap_datasets"
    This reverts commit af1a071e5234054a0f8cadaefa52be5feb1de2a5, reversing
    changes made to b91f8596393c6166eb9260985ae798ee4e6d2f22.

--- src/filebuilders/csv_builder.py
+++ src/filebuilders/csv_builder.py
@@ -4,18 +4,27 @@ import os
 
 class CSVBuilder(FileBuilder):
 
-    def build(self, file_number, data):
-        output_dir = self.get_output_directory()
-        file_name = self.get_file_name()
+    def build(self, file_extension, data, domain_object_config):                      
+        file_name = domain_object_config['file_name'] + '_{0}' + file_extension
+        output_dir = domain_object_config['output_directory']
 
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
 
-        with open(os.path.join(output_dir, 
-                  file_name.format(f'{file_number:03}')),
-                  'w+', newline='') as output_file:
-            dict_writer = csv.DictWriter(output_file, restval="-",
-                                         fieldnames=data[0].keys(),
-                                         delimiter=',')
-            dict_writer.writeheader()
-            dict_writer.writerows(data)
+        start = 0
+        max_objects_per_file = int(domain_object_config['max_objects_per_file'])
+        file_count = max(int(len(data) / max_objects_per_file), 1)
+        upload_to_google_drive = domain_object_config['upload_to_google_drive']
+
+        for i in range(0, file_count):
+            current_slice = data[start : start + max_objects_per_file]
+            file_name = file_name.format(f'{i+1:03}')
+            with open(os.path.join(output_dir, file_name), 'w+', newline='') as output_file:
+                dict_writer = csv.DictWriter(output_file, restval="-", fieldnames=data[0].keys(), delimiter=',')
+                dict_writer.writeheader()
+                dict_writer.writerows(current_slice)
+
+            if upload_to_google_drive.upper() == 'TRUE':
+                self.upload_to_google_drive(output_dir, file_name)
+            
+            start += max_objects_per_file
