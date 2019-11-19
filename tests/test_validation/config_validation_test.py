2fdba487b8dc7b5000d0cbd48e3762cb7d1dd888
WilfGala <wilfrid.berry@galatea-associates.com>
2019-11-19 15:23:40 +0000

    issue-167: hound fix

--- tests/test_validation/config_validation_test.py
+++ tests/test_validation/config_validation_test.py
@@ -1,8 +1,8 @@
 import copy
 from configuration import configuration
 from validator import config_validator as validator
-from tests.resources.config_files.configuration_objects \
-    import default_gen_arguments, default_domain_objects, \
+from tests.resources.config_files.configuration_objects\
+    import default_gen_arguments, default_domain_objects,\
     default_shared_arguments, default_file_builders
 """ 
 In the main method of src/app.py, the user and dev JSON config files are
@@ -10,15 +10,15 @@ converted to Python objects using the ujson library prior to validation. The
 objects loaded from JSON are then bundled into a dictionary and passed to the
 constructor of the Configuration class to create a Configuration object.
 
-The Configuration object is then passed to the validate method of the 
+The Configuration object is then passed to the validate method of the
 validate.py module. This is the method we are testing in these test cases. As
 such, a Configuration object is mocked appropriately for each test case.
 
-Test stubs of the Python objects created by ujson are imported from 
-tests/resources/config_files/configuration_objects.py. 
-These are valid by default, and represent the use of a single domain object 
-factory to output to a CSV file. To mock invalid configs, the relevant stub 
-is deep copied then modified to introduce an invalid parameter. 
+Test stubs of the Python objects created by ujson are imported from
+tests/resources/config_files/configuration_objects.py.
+These are valid by default, and represent the use of a single domain object
+factory to output to a CSV file. To mock invalid configs, the relevant stub
+is deep copied then modified to introduce an invalid parameter.
 
 For more info on deep copying and the copy library see here:
 https://www.programiz.com/python-programming/shallow-deep-copy
