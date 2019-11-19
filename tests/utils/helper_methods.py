334192e83d482e8033ae498b156358b4bee8e08b
Thomas Edwards <edwards@galatea-associates.com>
2019-11-19 14:17:49 +0000

    #61 Update method names in tests

--- tests/utils/helper_methods.py
+++ tests/utils/helper_methods.py
@@ -45,153 +45,153 @@ def query_db(table_name, attribute=None):
 
 
 # Generation Methods
-def generate_back_office_position(amount=1):
+def create_back_office_position(amount=1):
     obj = back_office_position_factory.BackOfficePositionFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_cash_balance(amount=1):
+def create_cash_balance(amount=1):
     obj = cash_balance_factory.CashBalanceFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_cashflow(amount=1):
+def create_cashflow(amount=1):
     config = get_configuration()
     cashflow_config = config['domain_objects'][11]
     obj = cashflow_factory.CashflowFactory(cashflow_config)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_counterparty(amount=1):
+def create_counterparty(amount=1):
     obj = counterparty_factory.CounterpartyFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_depot_position(amount=1):
+def create_depot_position(amount=1):
     obj = depot_position_factory.DepotPositionFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_front_office_position(amount=1):
+def create_front_office_position(amount=1):
     obj = front_office_position_factory.FrontOfficePositionFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_instrument(amount=1):
+def create_instrument(amount=1):
     obj = instrument_factory.InstrumentFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_order_execution(amount=1):
+def create_order_execution(amount=1):
     obj = order_execution_factory.OrderExecutionFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_price(amount=1):
+def create_price(amount=1):
     obj = price_factory.PriceFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_stock_loan_position(amount=1):
+def create_stock_loan_position(amount=1):
     obj = stock_loan_position_factory.StockLoanPositionFactory(None, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_swap_contract(amount=1):
+def create_swap_contract(amount=1):
     config = get_configuration()
     swap_contract_config = config['domain_objects'][9]
     obj = swap_contract_factory.SwapContractFactory(swap_contract_config, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
-def generate_swap_position(amount=1):
+def create_swap_position(amount=1):
     config = get_configuration()
     swap_position_config = config['domain_objects'][10]
     obj = swap_position_factory.SwapPositionFactory(swap_position_config, None)
-    return obj.generate(amount, 0)
+    return obj.create(amount, 0)
 
 
 # Set-up Methods
 def set_up_back_office_position_tests():
     delete_local_database()
-    generate_instrument(50)
-    records = generate_back_office_position(50)
+    create_instrument(50)
+    records = create_back_office_position(50)
     domain_obj = back_office_position_factory.BackOfficePositionFactory(None, None)
     return records, domain_obj
 
 
 def set_up_cash_balance_tests():
     domain_obj = cash_balance_factory.CashBalanceFactory(None, None)
-    records = generate_cash_balance(50)
+    records = create_cash_balance(50)
     return records, domain_obj
 
 
 def set_up_cashflow_tests():
     delete_local_database()
-    generate_instrument(50)
-    generate_counterparty(20)
-    generate_swap_contract(20)
-    generate_swap_position(40)
-    return generate_cashflow(20)
+    create_instrument(50)
+    create_counterparty(20)
+    create_swap_contract(20)
+    create_swap_position(40)
+    return create_cashflow(20)
 
 
 def set_up_counterparty_tests():
-    return generate_counterparty(50)
+    return create_counterparty(50)
 
 
 def set_up_depot_position_tests():
     delete_local_database()
-    generate_instrument(50)
-    records = generate_depot_position(50)
+    create_instrument(50)
+    records = create_depot_position(50)
     domain_obj = depot_position_factory.DepotPositionFactory(None, None)
     return records, domain_obj
 
 
 def set_up_front_office_position_tests():
     delete_local_database()
-    generate_instrument(50)
-    records = generate_front_office_position(50)
+    create_instrument(50)
+    records = create_front_office_position(50)
     domain_obj = front_office_position_factory.FrontOfficePositionFactory(None, None)
     return records, domain_obj
 
 
 def set_up_instrument_tests():
     delete_local_database()
-    return generate_instrument(50)
+    return create_instrument(50)
 
 
 def set_up_order_execution_tests():
     delete_local_database()
-    generate_instrument(50)
-    return generate_order_execution(50)
+    create_instrument(50)
+    return create_order_execution(50)
 
 
 def set_up_price_tests():
     delete_local_database()
-    generate_instrument(50)
-    return generate_price(50)
+    create_instrument(50)
+    return create_price(50)
 
 
 def set_up_stock_loan_position_tests():
     delete_local_database()
-    generate_instrument(50)
-    records = generate_stock_loan_position(50)
+    create_instrument(50)
+    records = create_stock_loan_position(50)
     domain_obj = stock_loan_position_factory.StockLoanPositionFactory(None, None)
     return records, domain_obj
 
 
 def set_up_swap_contract_tests():
     delete_local_database()
-    generate_counterparty(20)
-    records = generate_swap_contract(20)
+    create_counterparty(20)
+    records = create_swap_contract(20)
     domain_obj = swap_contract_factory.SwapContractFactory(None, None)
     return records, domain_obj
 
 
 def set_up_swap_position_tests():
-    generate_instrument(50)
-    generate_counterparty(20)
-    generate_swap_contract(20)
-    records = generate_swap_position(50)
+    create_instrument(50)
+    create_counterparty(20)
+    create_swap_contract(20)
+    records = create_swap_position(50)
     domain_obj = swap_position_factory.SwapPositionFactory(None, None)
     return records, domain_obj
