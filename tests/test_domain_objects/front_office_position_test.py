21d2835c9f0c62463ea1efb829d8b4df7a1a5b9c
WilfGala <wilfrid.berry@galatea-associates.com>
2019-11-25 16:22:02 +0000

    issue-179: added tests for front office position factory

--- tests/test_domain_objects/front_office_position_test.py
+++ tests/test_domain_objects/front_office_position_test.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 sys.path.insert(0, 'tests/')
 from utils import shared_tests as shared
 from utils import helper_methods as helper
@@ -7,16 +8,39 @@ from utils import helper_methods as helper
 def test_front_office_position():
     records, domain_obj = helper.set_up_front_office_position_tests()
     for record in records:
-        shared.attribute_quantity_valid('front_office_position', record, 9)
-        shared.ric_exists(record)
-        shared.position_type_valid(record)
-        shared.knowledge_date_valid(record)
-        if record['position_type'] == 'TD':
-            shared.trade_position_effective_date_valid(record)
-        else:
-            shared.settlement_position_effective_date_valid(record)
-        shared.account_valid(record, domain_obj.ACCOUNT_TYPES)
-        shared.direction_valid(record)
-        shared.quantity_valid(record)
-        shared.purpose_valid(record,
-                             domain_obj.FRONT_OFFICE_POSITION_PURPOSES)
+        shared.attribute_quantity_valid('front_office_position', record, 6)
+        as_of_date_valid(record)
+        value_date_valid(record)
+        account_id_valid(record, domain_obj)
+        cusip_valid(record, domain_obj)
+        quantity_valid(record)
+        purpose_valid(record)
+
+
+def as_of_date_valid(record):
+    assert record['as_of_date'] == datetime.date.today()
+
+
+def value_date_valid(record):
+    value_date = record['value_date']
+    today = datetime.date.today()
+    day_after_tomorrow = today + datetime.timedelta(days=2)
+    assert value_date in (today, day_after_tomorrow)
+
+
+def account_id_valid(record, domain_obj):
+    account_id = record['account_id']
+    assert account_id in domain_obj.retrieve_column('accounts', 'account_id')
+
+
+def cusip_valid(record, domain_obj):
+    cusip = record['cusip']
+    assert cusip in domain_obj.retrieve_column('instruments', 'cusip')
+
+
+def quantity_valid(record):
+    assert 1 <= abs(record['quantity']) <= 10000
+
+
+def purpose_valid(record):
+    assert record['purpose'] == 'Outright'
