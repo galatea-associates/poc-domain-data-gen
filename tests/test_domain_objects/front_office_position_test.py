9b3471cbc9a5567151f0267df0c4ad1dc9d33f47
WilfGala <wilfrid.berry@galatea-associates.com>
2019-11-25 17:53:58 +0000

    issue-179: added test docstrings

--- tests/test_domain_objects/front_office_position_test.py
+++ tests/test_domain_objects/front_office_position_test.py
@@ -6,6 +6,8 @@ from utils import helper_methods as helper
 
 
 def test_front_office_position():
+    """ Ensure all generated front office position attributes adhere to their
+    specification. """
     records, domain_obj = helper.set_up_front_office_position_tests()
     for record in records:
         shared.attribute_quantity_valid('front_office_position', record, 6)
@@ -18,10 +20,12 @@ def test_front_office_position():
 
 
 def as_of_date_valid(record):
+    """ as of date must be the current date """
     assert record['as_of_date'] == datetime.date.today()
 
 
 def value_date_valid(record):
+    """ value date must be either the current date or 2 days later """
     value_date = record['value_date']
     today = datetime.date.today()
     day_after_tomorrow = today + datetime.timedelta(days=2)
@@ -29,18 +33,23 @@ def value_date_valid(record):
 
 
 def account_id_valid(record, domain_obj):
+    """ account id must exist in the database 'accounts' table """
     account_id = record['account_id']
     assert account_id in domain_obj.retrieve_column('accounts', 'account_id')
 
 
 def cusip_valid(record, domain_obj):
+    """ cusip must exist in the database 'instruments' table """
     cusip = record['cusip']
     assert cusip in domain_obj.retrieve_column('instruments', 'cusip')
 
 
 def quantity_valid(record):
+    """ quantity must be positive or negative and have magnitude less than
+    or equal to 10000"""
     assert 1 <= abs(record['quantity']) <= 10000
 
 
 def purpose_valid(record):
+    """ purpose must be 'Outright' """
     assert record['purpose'] == 'Outright'
