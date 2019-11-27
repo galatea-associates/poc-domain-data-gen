6e4d7c202cdea68fcaec004ff715cc829324b2d3
WilfGala <wilfrid.berry@galatea-associates.com>
2019-11-27 09:16:13 +0000

    issue-176: renamed test files and functions

--- tests/test_domain_objects/trade_test.py
+++ tests/test_domain_objects/trade_test.py
@@ -4,11 +4,11 @@ from utils import shared_tests as shared
 from utils import helper_methods as helper
 
 
-def test_order_executions():
-    records = helper.set_up_order_execution_tests()
+def test_trades():
+    records = helper.set_up_trades()
     shared.unique_ids(records, 'order')
     for record in records:
-        shared.attribute_quantity_valid('order_execution', record, 10)
+        shared.attribute_quantity_valid('trade', record, 10)
         shared.account_number_valid(record)
         shared.direction_valid(record)
         sto_id_valid(record)
